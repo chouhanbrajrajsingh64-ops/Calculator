6d5359208bbdc5087da1ec56d4489f9e36f94afc
Brajraj Singh Chouhan <chouhanbrajrajsingh64@gmail.com>
2025-12-24 16:02:44 +0530

    Fix layout structure and mount footer correctly

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,12 @@
-import { Stack } from 'expo-router'
-import { ThemeProvider } from '@/ui/theme/ThemeContext'
+import { Stack } from "expo-router";
+import { ThemeProvider } from "@/ui/theme/ThemeContext";
+import AppFooter from "../ui/components/AppFooter";
 
 export default function Layout() {
   return (
     <ThemeProvider>
       <Stack screenOptions={{ headerShown: false }} />
+      <AppFooter />
     </ThemeProvider>
-  )
+  );
 }
